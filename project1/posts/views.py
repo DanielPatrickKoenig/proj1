6442e84ecf022f498065c8d896743dcb598b19d8
dan koenig <dangerbird@dans-MacBook-Pro.local>
2024-06-16 09:44:33 -0400

    iterating content and admin access

--- project1/posts/views.py
+++ project1/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 def posts_list (request):
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/posts_list.html', { 'posts': posts })
 
 # Create your views here.

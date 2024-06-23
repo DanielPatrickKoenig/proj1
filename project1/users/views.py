0d985e55593ee0356a56a9cbfccb89985c7f6246
dan koenig <dangerbird@dans-MacBook-Pro.local>
2024-06-22 21:02:42 -0400

    login form

--- project1/users/views.py
+++ project1/users/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 def register_user (request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("/posts")
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', { 'form': form })
+
+def login_user (request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("/posts")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', { 'form': form })
 
 # Create your views here.
